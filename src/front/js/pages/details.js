fc06f5c63c7ba8fa29f668675d4fbd5a0272efaa
kakum3 <kakum_3@hotmail.com>
2022-08-11 00:17:10 +0000

    detalles

--- src/front/js/pages/details.js
+++ src/front/js/pages/details.js
@@ -1,75 +1,93 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import Servicelist from "../component/servicelist";
-import "../../styles/details.css";
 
 export const Details = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="contain-fluid">
-    
-    <div className="row">
-      <div className="col-sm-12 col-lg-5 bg-dark">
-      <h2 className="logo">Taller <span>Paco</span></h2>
-        <Servicelist />
-        
-        <Servicelist />
+    <div className="container card m-auto bg-transparent text-white border-0">
+      <div className="row py-3 ">
+        <div className="col-sm-12 col-lg-6 ">
+          <h1 className="logo text-white "><span className="taller text-dark shadow-sm p-3 mb-4 bg-success rounded m-1">  Taller</span>
+           <span className="nombreTaller text-success shadow-sm p-3 mb-5 bg-dark rounded">Paco</span>
+          </h1>
+          <ul className="mt-5">
+            <li>
+              <i className="fas fa-map-marker-alt"></i> Baker Street
+            </li>
+            <li>
+              <i className="fas fa-phone"></i> (111) 111 111 111
+            </li>
+            <li>
+              <i className="fas fa-envelope-open-text"></i> contact@website.com
+            </li>
+          </ul>
 
+          <button
+            type="button"
+            className="btn btn-dark mt-5 text-white shadow-sm p-3 mb-5 bg-success rounded"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            data-bs-whatever="@mdo"
+          >
+          Contactar Taller
+          </button>
+
+          <div
+            className="modal fade text-center text-primary"
+            id="exampleModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog modal-dialog-centered  ">
+              <div className="modal-content">
+               
+                <div className="modal-body">
+                  <form>
+                    <div className="mb-3 mb-2 bg-transparentt">
+                      <label for="recipient-name " className="col-form-label">
+                       email:
+                       
+                      </label>
+                      <input
+                        type="email"
+                        className="form-control "
+                        id="recipient-name"
+                      ></input>
+                    </div>
+                    <div className="mb-3">
+                      <label for="message-text" className="col-form-label">
+                        Message:
+                      </label>
+                      <textarea
+                        className="form-control "
+                        id="message-text"
+                      ></textarea>
+                    </div>
+                  </form>
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="button" className="btn btn-primary">
+                    Send 
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-12 col-lg-5">
+          <Servicelist />
+        </div>
       </div>
-      <div className="col-sm-12 col-lg-7">
-       
-            <div className="content">
-
-<h1 className="logo">Contact <span>Us</span></h1>
-
-<div className="contact-wrapper animated bounceInUp">
-    <div className="contact-form">
-        <h3>Contact us</h3>
-        <form action="">
-            <p>
-                <label>FullName</label>
-                <input type="text" name="fullname"/>
-            </p>
-            <p>
-                <label>Email Adress</label>
-                <input type="email" name="email"/>
-            </p>
-            <p>
-                <label>Phone Number</label>
-                <input type="tel" name="phone"/>
-            </p>
-            <p>
-                <label>Affair</label>
-                <input type="text" name="affair"/>
-            </p>
-            <p className="block">
-               <label>Message</label> 
-                <textarea name="message" rows="3"></textarea>
-            </p>
-            <p className="block">
-                <button>
-                    Send
-                </button>
-            </p>
-        </form>
     </div>
-    <div className="contact-info">
-        <h4>TALLER PACO</h4>
-        <ul>
-            <li><i className="fas fa-map-marker-alt"></i> Baker Street</li>
-            <li><i className="fas fa-phone"></i> (111) 111 111 111</li>
-            <li><i className="fas fa-envelope-open-text"></i> contact@website.com</li>
-        </ul>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero provident ipsam necessitatibus repellendus?</p>
-        <p>Company.com</p>
-    </div>
-</div>
-</div>
-</div>
-
-      </div>
-    </div>
-    
   );
 };
